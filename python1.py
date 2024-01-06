877ec37a529a0013a36c43cd5048134e08f223e5
Preety Banjare <52145310+goonjapreet@users.noreply.github.com>
2024-01-06 13:55:46 +0530

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,26 @@
+"""Create a class named 'Student' with String variable 'name' and integer variable 'roll_no'.
+
+Create a constructor through which you can assign values through objects on creation of objects.
+
+if No value is passed through object then by default name should be initialized to "john" and roll_no as 2
+"""
+
+def main():
+    
+    
+    print("I am a girl")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
